92a8e9acd12359afd76c2c71c1b11d12935f2247
ocph23 <ocph23@hotmail.com>
2018-08-09 08:41:03 +0900

    FIrstCommit

---
+++ Penjadwalan/Penjadwalan/Controllers/MatrikController.cs
@@ -0,0 +1,47 @@
+﻿using Penjadwalan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Penjadwalan.Controllers
+{
+    public class MatrikController : ApiController
+    {
+        public Data Get()
+        {
+            return GetData();
+        }
+
+        private Data GetData()
+        {
+            Data d = new Data();
+            d.Nodes = GetNodes();
+            return d;
+        }
+
+        private List<Node> GetNodes()
+        {
+            var list = new List<Node>();
+            list.Add(new Node() { id = 1, label = "Senin Pagi", shape="Oval" , font=new font { color="white" }, color=new color {  background="red" } });
+            list.Add(new Node() { id = 2, label = "Senin Malam", shape = "Oval", font = new font { color = "white" }, color = new color { background = "green" } });
+            list.Add(new Node() { id = 3, label = "Selasa Pagi", shape = "Oval", font = new font { color = "white" }, color = new color { background = "red" } });
+            list.Add(new Node() { id = 4, label = "Selasa Malam", shape = "Oval", font = new font { color = "white" }, color = new color { background = "green" } });
+            list.Add(new Node() { id = 5, label = "Rabu Pagi", shape = "Oval", font = new font { color = "white" }, color = new color { background = "red" } });
+            list.Add(new Node() { id = 6, label = "Rabu Malam", shape = "Oval", font = new font { color = "white" }, color = new color { background = "green" } });
+            list.Add(new Node() { id = 7, label = "Kamis Pagi", shape = "Oval", font = new font { color = "white" }, color = new color { background = "red" } });
+            list.Add(new Node() { id = 8, label = "Kamis Malam", shape = "Oval", font = new font { color = "white" }, color = new color { background = "green" } });
+            list.Add(new Node() { id = 9, label = "Jumat Pagi", shape = "Oval", font = new font { color = "white" }, color = new color { background = "red" } });
+            list.Add(new Node() { id = 10, label = "Jumat Malam", shape = "Oval", font = new font { color = "white" }, color = new color { background = "green" } });
+            list.Add(new Node() { id = 11, label = "Sabtu Pagi", shape = "Oval", font = new font { color = "white" }, color = new color { background = "red" } });
+            list.Add(new Node() { id = 12, label = "Sabtu Malam", shape = "Oval", font = new font { color = "white" }, color = new color { background = "green" } });
+            list.Add(new Node() { id = 13, label = "Minggu Pagi", shape = "Oval", font = new font { color = "white" }, color = new color { background = "red" } });
+            list.Add(new Node() { id = 14, label = "Minggu Malam", shape = "Oval", font = new font { color = "white" }, color = new color { background = "green" } });
+
+            return list;
+
+        }
+    }
+}
---
+++ Penjadwalan/Penjadwalan/Models/Node.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Penjadwalan.Models
+{
+    public class Node
+    {
+        public int id { get; set; }
+
+        public string label { get; set; }
+        public string shape { get; set; }
+        public color color { get; set; }
+        public font font { get; set; }
+
+    }
+
+
+    public class Data
+    {
+        public List<Node> Nodes { get; set; }
+        public List<Adge> Adges { get; set; }
+    }
+
+
+    public class Adge
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public string label { get; set; }
+        public string arrows { get; set; }
+    }
+
+    public class color
+    {
+        public string background { get; set; }
+        public string border { get; set; }
+    }
+
+
+    public class font
+    {
+        public string color { get; set; }
+    }
+}
